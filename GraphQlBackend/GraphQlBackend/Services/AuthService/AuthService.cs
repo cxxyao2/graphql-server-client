b5db6514c9a0a1be74591b2218fc010ee402a154
cheng jennifer <cheng2024xx@outlook.com>
2024-08-07 01:27:21 +0000

    Updated AuthService.cs

--- GraphQlBackend/GraphQlBackend/Services/AuthService/AuthService.cs
+++ GraphQlBackend/GraphQlBackend/Services/AuthService/AuthService.cs
@@ -76,7 +76,7 @@ namespace GraphQlBackend.Services
 
         private async Task<TokenPair> GenerateTokens(User user)
         {
-            string? tokenSecret = _config["AppSettings:Secrets_Token"];
+            string? tokenSecret = _config["Secrets_Token"];
             if (string.IsNullOrEmpty(tokenSecret))
             {
                 throw new Exception("Token secret is not set");
